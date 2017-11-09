689227adc46d1fb5fb5b74e6b46599a3bc2ed69c
Chase Paules <cpaules@gmail.com>
2017-11-09 21:12:54 +0000

    automatically backed up by learn

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+p1 = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.", price: "2.99")
+p2 = Product.create!(name: "Test Product 2", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
